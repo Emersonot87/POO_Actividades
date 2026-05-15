b90a95ac70e0bb648565fb3a957f551400df4142
Emersonot87 <emotalvaroh@unal.edu.co>
2026-05-15 17:52:21 -0500

    solved actividad 3

---
+++ Actividad #3/ejercicio 8.2/solucion/Main.java
@@ -0,0 +1,11 @@
+package solucion;
+
+import solucion.interfaz.VentanaPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
+
